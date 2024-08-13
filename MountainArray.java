b10e308d07fc70b83ae531014bba044963a43a4b
teliharshal <133125778+teliharshal@users.noreply.github.com>
2024-08-13 17:36:49 +0530

    Add files via upload

---
+++ MountainArray.java
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+public class MountainArray {
+    public static void main(String[] args) {
+        int[] arr={0,10,5,2};
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+    public static int peakIndexInMountainArray(int[] arr) {
+        int start=0;
+        int end=arr.length-1;
+
+        while(start < end)
+        {
+            int mid = start + (end-start) /2;
+            if(arr[mid] > arr[mid+1])
+            {
+                end=mid;
+            }
+            else{
+                start =mid+1;
+            }
+        }
+        return start;
+
+    }
+
+}
